0d58c5719779db7f2a8344cccdfa3d829d84de37
Suraj P S <surajps@gmail.com>
2025-11-07 16:05:20 +0530

    Fix Typo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -28,7 +28,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SURAJ</strong>
+                <strong className="main-name"> SURAJ P S</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
